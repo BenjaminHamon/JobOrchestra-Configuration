4d1fc63a6d637d28f3a15957024c9f6463267167
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-06-29 22:21:36 +0200

    Refactor build filters for project status

---
+++ website_extensions/bhamon_build_website_extensions/context_provider.py
@@ -0,0 +1,145 @@
+def get_project_context(project, context):
+	if project == "build-service":
+		return get_build_service_context(context)
+	if project == "image-manager":
+		return get_image_manager_context(context)
+	if project == "my-website":
+		return get_my_website_context(context)
+	if project == "solitaire":
+		return get_solitaire_context(context)
+	raise ValueError("Unknown project: '%s'" % project)
+
+
+def get_build_service_context(context): 
+	project_filters = get_build_service_filters()
+
+	if context is None:
+		return [ f for f in project_filters ]
+	raise ValueError("Unknown context '%s' for project '%s'" % (context, "build-service"))
+
+
+def get_build_service_filters():
+	return [
+		{
+			"identifier": "check_linux",
+			"display_name": "Check Linux",
+			"job_identifier": "build-service_check_linux",
+		},
+		{
+			"identifier": "check_windows",
+			"display_name": "Check Windows",
+			"job_identifier": "build-service_check_windows",
+		},
+		{
+			"identifier": "distribute",
+			"display_name": "Distribution",
+			"job_identifier": "build-service_distribute",
+		},
+	]
+
+
+def get_image_manager_context(context): 
+	project_filters = get_image_manager_filters()
+
+	if context is None:
+		return [ f for f in project_filters ]
+	raise ValueError("Unknown context '%s' for project '%s'" % (context, "image-manager"))
+
+
+def get_image_manager_filters():
+	return [
+		{
+			"identifier": "controller",
+			"display_name": "Controller",
+			"job_identifier": "image-manager_controller",
+		},
+		{
+			"identifier": "package_debug",
+			"display_name": "Package (Debug)",
+			"job_identifier": "image-manager_package",
+			"condition": lambda build: build["parameters"]["configuration"] == "Debug",
+		},
+		{
+			"identifier": "package_release",
+			"display_name": "Package (Release)",
+			"job_identifier": "image-manager_package",
+			"condition": lambda build: build["parameters"]["configuration"] == "Release",
+		},
+	]
+
+
+def get_my_website_context(context): 
+	project_filters = get_my_website_filters()
+
+	if context is None:
+		return [ f for f in project_filters ]
+	raise ValueError("Unknown context '%s' for project '%s'" % (context, "my-website"))
+
+
+def get_my_website_filters():
+	return [
+		{
+			"identifier": "check",
+			"display_name": "Check",
+			"job_identifier": "my-website_check",
+		},
+		{
+			"identifier": "distribute",
+			"display_name": "Distribution",
+			"job_identifier": "my-website_distribute",
+		},
+	]
+
+
+def get_solitaire_context(context): 
+	project_filters = get_solitaire_filters()
+
+	if context is None:
+		return [ f for f in project_filters ]
+	raise ValueError("Unknown context '%s' for project '%s'" % (context, "solitaire"))
+
+
+def get_solitaire_filters():
+	return [
+		{
+			"identifier": "controller",
+			"display_name": "Controller",
+			"job_identifier": "solitaire_controller",
+		},
+		{
+			"identifier": "package_android_debug",
+			"display_name": "Android Package (Debug)",
+			"job_identifier": "solitaire_package_android",
+			"condition": lambda build: build["parameters"]["configuration"] == "Debug",
+		},
+		{
+			"identifier": "package_android_release",
+			"display_name": "Android Package (Release)",
+			"job_identifier": "solitaire_package_android",
+			"condition": lambda build: build["parameters"]["configuration"] == "Release",
+		},
+		{
+			"identifier": "package_linux_debug",
+			"display_name": "Linux Package (Debug)",
+			"job_identifier": "solitaire_package_linux",
+			"condition": lambda build: build["parameters"]["configuration"] == "Debug",
+		},
+		{
+			"identifier": "package_linux_release",
+			"display_name": "Linux Package (Release)",
+			"job_identifier": "solitaire_package_linux",
+			"condition": lambda build: build["parameters"]["configuration"] == "Release",
+		},
+		{
+			"identifier": "package_windows_debug",
+			"display_name": "Windows Package (Debug)",
+			"job_identifier": "solitaire_package_windows",
+			"condition": lambda build: build["parameters"]["configuration"] == "Debug",
+		},
+		{
+			"identifier": "package_windows_release",
+			"display_name": "Windows Package (Release)",
+			"job_identifier": "solitaire_package_windows",
+			"condition": lambda build: build["parameters"]["configuration"] == "Release",
+		},
+	]
--- website_extensions/bhamon_build_website_extensions/project_controller.py
+++ website_extensions/bhamon_build_website_extensions/project_controller.py
@@ -3,6 +3,7 @@ import logging
 import flask
 
 import bhamon_build_website.service_client as service_client
+import bhamon_build_website_extensions.context_provider as context_provider
 
 
 logger = logging.getLogger("ProjectController")
@@ -10,9 +11,10 @@ logger = logging.getLogger("ProjectController")
 
 def project_index(project_identifier):
 	branch = flask.request.args.get("branch", default = None)
+	context_identifier = flask.request.args.get("context", default = None)
 	status_limit = max(min(flask.request.args.get("limit", default = 20, type = int), 100), 1)
 	access_token = flask.request.args.get("access_token", default = None)
-	group_collection = get_status_groups(project_identifier)
+	project_context = context_provider.get_project_context(project_identifier, context_identifier)
 
 	status_parameters = {
 		"branch": branch,
@@ -25,123 +27,16 @@ def project_index(project_identifier):
 	project_status = [ revision for revision in project_status if len(revision["builds"]) > 0 ][ : status_limit ]
 
 	for revision in project_status:
+		revision["builds_by_filter"] = { f["identifier"]: [] for f in project_context }
 		for build in revision["builds"]:
-			build["group"] = get_build_group(build, group_collection)
+			for build_filter in project_context:
+				if build["job"] == build_filter["job_identifier"] and build_filter.get("condition", lambda build: True)(build):
+					revision["builds_by_filter"][build_filter["identifier"]].append(build)
 
 	view_data = {
 		"project_identifier": project_identifier,
+		"project_context": project_context,
 		"project_status": project_status,
-		"group_collection": group_collection,
 	}
 
 	return flask.render_template("project/index.html", title = "Dashboard", **view_data)
-
-
-def get_build_group(build, group_collection):
-	for group in group_collection:
-		if build["job"] == group["job_identifier"] and group.get("condition", lambda build: True)(build):
-			return group["identifier"]
-	return None
-
-
-def get_status_groups(project_identifier):
-	if project_identifier == "build-service":
-		return [
-			{
-				"identifier": "check_linux",
-				"display_name": "Check Linux",
-				"job_identifier": "build-service_check_linux",
-			},
-			{
-				"identifier": "check_windows",
-				"display_name": "Check Windows",
-				"job_identifier": "build-service_check_windows",
-			},
-			{
-				"identifier": "distribute",
-				"display_name": "Distribution",
-				"job_identifier": "build-service_distribute",
-			},
-		]
-
-	if project_identifier == "image-manager":
-		return [
-			{
-				"identifier": "controller",
-				"display_name": "Controller",
-				"job_identifier": "image-manager_controller",
-			},
-			{
-				"identifier": "package_debug",
-				"display_name": "Package (Debug)",
-				"job_identifier": "image-manager_package",
-				"condition": lambda build: build["parameters"]["configuration"] == "Debug",
-			},
-			{
-				"identifier": "package_release",
-				"display_name": "Package (Release)",
-				"job_identifier": "image-manager_package",
-				"condition": lambda build: build["parameters"]["configuration"] == "Release",
-			},
-		]
-
-	if project_identifier == "my-website":
-		return [
-			{
-				"identifier": "check",
-				"display_name": "Check",
-				"job_identifier": "my-website_check",
-			},
-			{
-				"identifier": "distribute",
-				"display_name": "Distribution",
-				"job_identifier": "my-website_distribute",
-			},
-		]
-
-	if project_identifier == "solitaire":
-		return [
-			{
-				"identifier": "controller",
-				"display_name": "Controller",
-				"job_identifier": "solitaire_controller",
-			},
-			{
-				"identifier": "package_android_debug",
-				"display_name": "Android Package (Debug)",
-				"job_identifier": "solitaire_package_android",
-				"condition": lambda build: build["parameters"]["configuration"] == "Debug",
-			},
-			{
-				"identifier": "package_android_release",
-				"display_name": "Android Package (Release)",
-				"job_identifier": "solitaire_package_android",
-				"condition": lambda build: build["parameters"]["configuration"] == "Release",
-			},
-			{
-				"identifier": "package_linux_debug",
-				"display_name": "Linux Package (Debug)",
-				"job_identifier": "solitaire_package_linux",
-				"condition": lambda build: build["parameters"]["configuration"] == "Debug",
-			},
-			{
-				"identifier": "package_linux_release",
-				"display_name": "Linux Package (Release)",
-				"job_identifier": "solitaire_package_linux",
-				"condition": lambda build: build["parameters"]["configuration"] == "Release",
-			},
-			{
-				"identifier": "package_windows_debug",
-				"display_name": "Windows Package (Debug)",
-				"job_identifier": "solitaire_package_windows",
-				"condition": lambda build: build["parameters"]["configuration"] == "Debug",
-			},
-			{
-				"identifier": "package_windows_release",
-				"display_name": "Windows Package (Release)",
-				"job_identifier": "solitaire_package_windows",
-				"condition": lambda build: build["parameters"]["configuration"] == "Release",
-			},
-		]
-
-	raise ValueError("Unknown project: '%s'" % project_identifier)
