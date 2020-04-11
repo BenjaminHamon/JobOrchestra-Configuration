0cd2179906950873cf9d6ba8003564bc6e29c0c2
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-04-11 16:26:36 +0200

    Rename project field to project_identifier in configuration

--- development/configuration.py
+++ development/configuration.py
@@ -9,7 +9,7 @@ import sys
 
 def load_configuration(environment):
 	configuration = {
-		"project": "bhamon-orchestra-configuration",
+		"project_identifier": "bhamon-orchestra-configuration",
 		"project_name": "Job Orchestra Configuration",
 		"project_version": load_project_version(environment["git_executable"], "2.0"),
 	}
