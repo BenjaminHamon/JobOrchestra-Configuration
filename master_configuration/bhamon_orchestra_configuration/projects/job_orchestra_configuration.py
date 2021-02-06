f9cd3301d4e449b5a1041b7fa95c190bede66a64
Benjamin Hamon <hamon.benjamin@gmail.com>
2021-02-06 12:56:16 +0100

    Add per python version jobs for JobOrchestra-Configuration

--- master_configuration/bhamon_orchestra_configuration/projects/job_orchestra_configuration.py
+++ master_configuration/bhamon_orchestra_configuration/projects/job_orchestra_configuration.py
@@ -42,8 +42,12 @@ def configure_jobs():
 	return [
 		development_pipeline(),
 		release_pipeline(),
-		check("linux"),
-		check("windows"),
+		check("linux", "3.7"),
+		check("linux", "3.8"),
+		check("linux", "3.9"),
+		check("windows", "3.7"),
+		check("windows", "3.8"),
+		check("windows", "3.9"),
 		package(),
 		distribute(),
 	]
@@ -68,14 +72,38 @@ def development_pipeline():
 
 			"elements": [
 				{
-					"identifier": "check_linux",
-					"job": "check_linux",
+					"identifier": "check_linux_python-3.7",
+					"job": "check_linux_python-3.7",
 					"parameters": { "revision": "{results[revision_control][revision]}" },
 				},
 
 				{
-					"identifier": "check_windows",
-					"job": "check_windows",
+					"identifier": "check_linux_python-3.8",
+					"job": "check_linux_python-3.8",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_linux_python-3.9",
+					"job": "check_linux_python-3.9",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_windows_python-3.7",
+					"job": "check_windows_python-3.7",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_windows_python-3.8",
+					"job": "check_windows_python-3.8",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_windows_python-3.9",
+					"job": "check_windows_python-3.9",
 					"parameters": { "revision": "{results[revision_control][revision]}" },
 				},
 
@@ -85,8 +113,12 @@ def development_pipeline():
 					"parameters": { "revision": "{results[revision_control][revision]}" },
 
 					"after": [
-						{ "element": "check_linux", "status": "succeeded" },
-						{ "element": "check_windows", "status": "succeeded" },
+						{ "element": "check_linux_python-3.7", "status": "succeeded" },
+						{ "element": "check_linux_python-3.8", "status": "succeeded" },
+						{ "element": "check_linux_python-3.9", "status": "succeeded" },
+						{ "element": "check_windows_python-3.7", "status": "succeeded" },
+						{ "element": "check_windows_python-3.8", "status": "succeeded" },
+						{ "element": "check_windows_python-3.9", "status": "succeeded" },
 					],
 				},
 			],
@@ -124,14 +156,38 @@ def release_pipeline():
 
 			"elements": [
 				{
-					"identifier": "check_linux",
-					"job": "check_linux",
+					"identifier": "check_linux_python-3.7",
+					"job": "check_linux_python-3.7",
 					"parameters": { "revision": "{results[revision_control][revision]}" },
 				},
 
 				{
-					"identifier": "check_windows",
-					"job": "check_windows",
+					"identifier": "check_linux_python-3.8",
+					"job": "check_linux_python-3.8",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_linux_python-3.9",
+					"job": "check_linux_python-3.9",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_windows_python-3.7",
+					"job": "check_windows_python-3.7",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_windows_python-3.8",
+					"job": "check_windows_python-3.8",
+					"parameters": { "revision": "{results[revision_control][revision]}" },
+				},
+
+				{
+					"identifier": "check_windows_python-3.9",
+					"job": "check_windows_python-3.9",
 					"parameters": { "revision": "{results[revision_control][revision]}" },
 				},
 
@@ -141,8 +197,12 @@ def release_pipeline():
 					"parameters": { "revision": "{results[revision_control][revision]}" },
 
 					"after": [
-						{ "element": "check_linux", "status": "succeeded" },
-						{ "element": "check_windows", "status": "succeeded" },
+						{ "element": "check_linux_python-3.7", "status": "succeeded" },
+						{ "element": "check_linux_python-3.8", "status": "succeeded" },
+						{ "element": "check_linux_python-3.9", "status": "succeeded" },
+						{ "element": "check_windows_python-3.7", "status": "succeeded" },
+						{ "element": "check_windows_python-3.8", "status": "succeeded" },
+						{ "element": "check_windows_python-3.9", "status": "succeeded" },
 					],
 				},
 
@@ -171,16 +231,17 @@ def release_pipeline():
 	return job
 
 
-def check(platform):
+def check(platform, python_version):
 	initialization_entry_point = [ worker_python_executable, "-u", "-m", initialization_script ]
 	initialization_parameters = [ "--configuration", worker_configuration_path, "--results", "{result_file_path}" ]
 	initialization_parameters += [ "--type", "worker", "--repository", repository, "--revision", "{parameters[revision]}" ]
+	initialization_parameters += [ "--python-version", python_version ]
 	project_entry_point = [ ".venv/scripts/python", "-u", "development/main.py", "--verbosity", "debug", "--results", "{result_file_path}" ]
 
 	job = {
-		"identifier": "check_%s" % platform,
-		"display_name": "Check %s" % platform.capitalize(),
-		"description": "Run checks for the JobOrchestra-Configuration-Configuration project on %s." % platform.capitalize(),
+		"identifier": "check_%s_python-%s" % (platform, python_version),
+		"display_name": "Check for %s with Python %s" % (platform.capitalize(), python_version),
+		"description": "Run checks for the JobOrchestra-Configuration project on %s." % platform.capitalize(),
 
 		"definition": {
 			"type": "job",
