3c8c9dd21d90d71cb9a515b9c0dc77697b157a95
Benjamin Hamon <hamon.benjamin@gmail.com>
2021-03-07 15:24:12 +0100

    Handle database authentication for PostgreSQL

--- entry_points/factory.py
+++ entry_points/factory.py
@@ -17,7 +17,8 @@ def create_database_administration_factory(database_uri, database_authentication
 	if database_uri.startswith("mongodb://"):
 		return lambda: MongoDatabaseAdministration(pymongo.MongoClient(database_uri, **database_authentication))
 	if database_uri.startswith("postgresql://"):
-		database_engine = sqlalchemy.create_engine(database_uri)
+		database_uri_with_authentication = _add_authentication(database_uri, database_authentication)
+		database_engine = sqlalchemy.create_engine(database_uri_with_authentication)
 		return lambda: SqlDatabaseAdministration(database_engine.connect(), database_metadata)
 	raise ValueError("Unsupported database uri '%s'" % database_uri)
 
@@ -28,6 +29,19 @@ def create_database_client_factory(database_uri, database_authentication, databa
 	if database_uri.startswith("mongodb://"):
 		return lambda: MongoDatabaseClient(pymongo.MongoClient(database_uri, **database_authentication))
 	if database_uri.startswith("postgresql://"):
-		database_engine = sqlalchemy.create_engine(database_uri)
+		database_uri_with_authentication = _add_authentication(database_uri, database_authentication)
+		database_engine = sqlalchemy.create_engine(database_uri_with_authentication)
 		return lambda: SqlDatabaseClient(database_engine.connect(), database_metadata)
 	raise ValueError("Unsupported database uri '%s'" % database_uri)
+
+
+def _add_authentication(database_uri, database_authentication):
+	if database_authentication.get("username", None) is None:
+		return database_uri
+	if database_authentication.get("password", None) is None:
+		return database_uri
+
+	authentication_index = database_uri.index("://") + len("://")
+	authentication = database_authentication["username"] + ":" + database_authentication["password"]
+	authentication += "@localhost" if database_uri[authentication_index] == "/" else "@"
+	return database_uri[:authentication_index] + authentication + database_uri[authentication_index:]
